bafcbf05532b02a8502ff89ba2646932d451509b
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-10-29 12:49:31 -0400

    Maybe we have a mongodb connection now?

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -3,6 +3,16 @@
  * GET portfolio page
  */
 
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
 exports.create = function(req, res){
   res.render('portfolio', {title: "Michael Martin-Smucker: Code and Design for the Modern Web"});
 };
