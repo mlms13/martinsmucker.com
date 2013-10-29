41519c2991e16d2b3e9b1bbcfd489e61c5501bb4
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-10-29 00:08:33 -0400

    Create basic portfolio page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
+var portfolio = require('./routes/portfolio');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -37,6 +38,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/portfolio', portfolio.create);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/portfolio.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET portfolio page
+ */
+
+exports.create = function(req, res){
+  res.render('portfolio', {title: "Michael Martin-Smucker: Code and Design for the Modern Web"});
+};
