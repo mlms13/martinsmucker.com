f9d3156f5fb4abd2482fb08668f91a2e2ac9dd60
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-02-18 09:31:47 -0500

    Reorganize app.js
    This borrows a lot of the structure from my node-rook app.

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
+// import third-party modules
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    path = require('path');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var lessMiddleware = require('less-middleware');
-
-var app = express();
-
-// routes
-var index = require('./routes/index');
-var portfolio = require('./routes/portfolio');
-var blog = require('./routes/blog');
-var groceries = require('./routes/groceries');
-var error = require('./routes/error');
-
-// all environments
+// set up middleware for all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,7 +13,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-app.use(lessMiddleware({
+app.use(require('less-middleware')({
   src: __dirname + '/public/less',
   dest: __dirname + '/public/css',
   prefix: '/css',
@@ -36,11 +23,14 @@ app.use(lessMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// routes
+var index = require('./routes/index'),
+    portfolio = require('./routes/portfolio'),
+    blog = require('./routes/blog'),
+    groceries = require('./routes/groceries'),
+    error = require('./routes/error');
 
+// handle http requests
 app.get('/', index);
 app.get('/portfolio', portfolio);
 app.get('/blog', blog.list);
@@ -48,6 +38,7 @@ app.get('/blog/:slug', blog.showPost);
 app.get('/groceries', groceries);
 app.get('*', error);
 
-http.createServer(app).listen(app.get('port'), function(){
+// start listening for server activity
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
